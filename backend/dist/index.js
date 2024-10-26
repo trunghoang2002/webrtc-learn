abb387aba921ded22e7be9e0bf9e398b62cb263f
prad-human-007 <kanasepradhumn@gmail.com>
2024-10-26 23:58:18 +0530

    added the backend for socket io

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log("backend");
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = require("http");
+const socket_io_1 = require("socket.io");
+const httpServer = (0, http_1.createServer)();
+const io = new socket_io_1.Server(httpServer, {
+    cors: {
+        origin: "*"
+    }
+});
+io.on("connection", (socket) => {
+    console.log(`user connected ${socket}`);
+});
+httpServer.listen(3000, () => {
+    console.log("listening on port 3000");
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,17 @@
-console.log("backend")
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+
+const httpServer =  createServer();
+const io = new Server(httpServer, {
+    cors:{
+        origin: "*"
+    }
+});
+
+io.on("connection", (socket: Socket) => {
+    console.log(`user connected ${socket}`);
+});
+
+httpServer.listen(3000, () => {
+    console.log("listening on port 3000");
+});
