51a498a52383903501e19f5ece4eb779d8026783
prad-human-007 <kanasepradhumn@gmail.com>
2024-11-08 23:21:30 +0530

    added wrtc on server and create user to server meer connection complete

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import  express  from "express";
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
+const { RTCPeerConnection, RTCSessionDescription } = require('@roamhq/wrtc')
 
 const app = express();
 const httpServer = createServer(app);
@@ -10,11 +11,59 @@ const io = new Server(httpServer, {
     }
   });
 
+const users: any = {}
+
 io.on("connection", (socket: Socket) => {
   console.log("Connected Socket: ", socket.id);
 
+  const peerConnection: RTCPeerConnection= new RTCPeerConnection({
+    iceServers: [{ urls : ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"] } ]
+  });
+
+  users[socket.id] = { peerConnection };
+
+   // getting server Ice candidates
+   peerConnection.addEventListener("icecandidate", (event) => {
+    if(event.candidate) {
+      //emit candidates to the user
+      socket.emit("server-ice-candidate", event.candidate)
+    }
+  });
+
+
+  //Handle incomming client ice candidates
+  socket.on("client-ice-candidate", async (candidate: RTCIceCandidate) => {
+    try {
+      await peerConnection.addIceCandidate(candidate);
+    } catch (error) {
+      console.log("Error Receiving ICE candidates")
+    }
+  });
+
+  //handle SDP offer comming from the client
+  socket.on("client-offer", async (offer: RTCSessionDescription) => {
+    await peerConnection.setRemoteDescription(offer);
+
+    //creating answer
+    const answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer);
+
+    socket.emit("server-answer", answer);
+  });
+
+  // handling track received from the client 
+  peerConnection.addEventListener("track", (event) => {
+    console.log("Received audio track from user");
+
+    const sender = peerConnection.addTrack(event.track);
+  });
+
   socket.on("disconnect", (reason) => {
-    console.log("Disconnected socket: ", socket.id, reason );
+    console.log("Disconect socket: ", socket.id, reason );
+    if(peerConnection)  {
+      peerConnection.close()
+      console.log("Closed Peer connection")
+    }
   });
   
 });
--- frontend/src/components/Video.tsx
+++ frontend/src/components/Video.tsx
@@ -6,27 +6,99 @@ import { io, Socket } from "socket.io-client";
 const URL = "http://localhost:3000"
 
 export default function Video() {
-    const [_, setSocket] = useState<Socket | null>(null);
+    const socketRef = useRef<Socket | null>(null)
+    const peerConnectionRef = useRef<RTCPeerConnection | null>(null)
+    const localStreamRef = useRef<MediaStream | null>(null)
+    const localAudioRef = useRef<HTMLAudioElement | null>(null)
+    const remoteAudioRef = useRef<HTMLAudioElement | null>(null);
     const [socketId, setSocketId] = useState("");
 
-    useEffect(()=> {
-        const socketInstance = io(URL)
-        setSocket(socketInstance)
+    useEffect(() => {
+        // setup websocket connection 
+        socketRef.current = io(URL);
+        socketRef.current.on("connect", () => {
+            console.log("Socket Connected: ", socketRef.current?.id )
+            if(socketRef.current)
+                setSocketId(socketRef.current.id!)
+        });
+    
+        return () => {
+            disconect();
+        }
 
-        socketInstance.on("connect", () => {
-            setSocketId(socketInstance.id!)
-            console.log("Connected to socket ", socketInstance.id );
+    }, []);
+    
+    const connect = async () => {
+
+        // create RTC peer connection 
+        peerConnectionRef.current= new RTCPeerConnection({
+            iceServers : [{ urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"] }]
+        })
+        
+        // Handle server Answer, Icecandidates and tracks (Server To Local)
+        socketRef.current?.on('server-answer', (answer: RTCSessionDescription) => {
+            console.log("Got answer from server");
+            if(answer)
+                peerConnectionRef.current?.setRemoteDescription(answer);
         });
 
-        return () => {
-            console.log("Disconnected")
-            socketInstance.disconnect()
+        socketRef.current?.on('server-ice-candidate', (serverIceCandidate: RTCIceCandidate) => {
+            console.log("Got Ice candidate from server");
+            if(serverIceCandidate)
+                peerConnectionRef.current?.addIceCandidate(serverIceCandidate);
+        });
+
+        peerConnectionRef.current.addEventListener('track', (event) => {
+            console.log("Got track from the server");
+            const serverStream: MediaStream = event.streams[0];
+            if(remoteAudioRef.current)
+                remoteAudioRef.current.srcObject = serverStream
+
+        });
+
+        // get local audio stream and attach it to the HTMLAudioEle and sedd to the server
+        // and send ICE candidates to the server and create offer (Local to Server)
+        const localStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        localStreamRef.current = localStream
+        if(localAudioRef.current)
+            localAudioRef.current.srcObject = localStream
+
+        localStream.getTracks().forEach((track) => {
+            peerConnectionRef.current?.addTrack(track)
+        });
+
+        peerConnectionRef.current.addEventListener("icecandidate", (event) => {
+            console.log("Got ICE candidates form the server");
+            if(event.candidate)
+                socketRef.current?.emit("client-ice-candidate", event.candidate)
+        });
+
+        const offer = await peerConnectionRef.current.createOffer();
+        await peerConnectionRef.current.setLocalDescription(offer);
+        socketRef.current?.emit("client-offer", offer);
+
+    }
+
+    const disconect = async () => {
+        if(peerConnectionRef.current){
+            peerConnectionRef.current.close()
+            peerConnectionRef.current = null;
         }
-    }, []);
+        socketRef.current?.disconnect();
+        if(remoteAudioRef.current)
+            remoteAudioRef.current.srcObject = null
+        if(localAudioRef.current)
+            localAudioRef.current.srcObject = null
+        setSocketId("")
+    }
 
     return (
         <>
         <h1> Connected with Socket Id: {socketId} </h1>
+        <button onClick={connect}> Connect </button>
+        <button onClick={disconect}> Disconnect </button>
+        <audio ref={localAudioRef} autoPlay />
+        <audio ref={remoteAudioRef} autoPlay />
         </>
     )
 }
