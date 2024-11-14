bb49af8ce25ed3e080cf75d4b296679b1f17e518
prad-human-007 <kanasepradhumn@gmail.com>
2024-11-14 12:52:13 +0530

    converted the python backend server into just an webrtc server which takes offer as a post message. Removed Web socket

--- backend-python/main.py
+++ backend-python/main.py
@@ -1,11 +1,11 @@
 import asyncio
-from aiortc import RTCPeerConnection, RTCSessionDescription, RTCIceServer, RTCConfiguration,RTCIceCandidate
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from aiortc import RTCPeerConnection, RTCSessionDescription, RTCIceServer, RTCConfiguration,RTCIceCandidate, MediaStreamTrack
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
 import uvicorn
 import logging
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("my_logger")
+logger = logging.getLogger("server")
 
 # create FastAPI app
 app = FastAPI()
@@ -14,71 +14,36 @@ app = FastAPI()
 async def read_root():
     return {"message": "Professional Moterboater"}
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    logger.info("Client connected")
-    print("Client connected")
-    try:
-        while True:
-            data = await websocket.receive_text()
-            await websocket.send_text(f"Message text was: {data}")
-    except WebSocketDisconnect:
-        print("Client disconnected")
+@app.post("/")
+async def offer(request: Request):
+    params = await request.json()
+    offer = RTCSessionDescription(type=params["type"], sdp=params["sdp"])
 
-@app.websocket("/wsRTC")
-async def websocket_rtc(websocket: WebSocket):
-    await websocket.accept()
-    print("WS Client connected")
+    pc = RTCPeerConnection()
+    logger.info("Created PeerConnection Object for: ", request.client.host)
+
+    @pc.on("iceconnectionstatechange")
+    async def on_iceconnectionstatechange():
+        logger.info("ICE connection state is: ", pc.iceConnectionState)
+        if pc.iceConnectionState == "failed":
+            await pc.close()
+            logger.info("PeerConnection closed")
     
-    config = RTCConfiguration(iceServers=[
-        RTCIceServer(urls="stun:stun.l.google.com:19302"),
-        RTCIceServer(urls="stun:stun1.l.google.com:19302"),
-    ])
-    pc = RTCPeerConnection(configuration=config)
-
-    async def on_icecandidate(candidate):
-        print("Got server ice candidate: ", candidate)
-        await websocket.send_json({"type": "candidate", 
-                                   "candidate": candidate})
-    pc.onicecandidate = on_icecandidate
-
     @pc.on("track")
-    def on_track(track):
-        print("Track received")
+    async def on_track(track: MediaStreamTrack):
+        logger.info(f"Track received: {track} of kind {track.kind}" )
         pc.addTrack(track)
+    
+    await pc.setRemoteDescription(offer)
+    answer = pc.createAnswer()
+    await pc.setLocalDescription(answer)
 
-    try:
-        while True:
-            message = await websocket.receive_json()
-            if message["type"] == "offer":
-                offer = RTCSessionDescription(sdp=message["sdp"], type=message["type"])
-                print("Received offer")
-                await pc.setRemoteDescription(offer)
-                answer = await pc.createAnswer()
-                await pc.setLocalDescription(answer)
-                await websocket.send_json({"sdp": pc.localDescription.sdp, "type": pc.localDescription.type})
-            elif message["type"] == "candidate":
-                try:
-                    candidate = message["candidate"]
-                    print("candidate from client: ", candidate)
-                    ice_candidate = RTCIceCandidate(
-                        component=candidate["candidate"], 
-                        sdpMid=candidate["sdpMid"], 
-                        sdpMLineIndex=candidate["sdpMLineIndex"],)
-                    print("Type of candidate: ", type(candidate))
-                    # await pc.addIceCandidate(candidate)
-                except Exception as e:
-                    print("Exception: ", e)
+    logger.info("Answer: ", pc.localDescription)
 
-    except WebSocketDisconnect:
-        print("RTC Client disconnected")
-        await pc.close()
-    except Exception as e:
-        print("Custom Exception: ", e)
-        await pc.close()
+    return {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
 
-app.websocket("/wsRTC")(websocket_rtc)
+
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
