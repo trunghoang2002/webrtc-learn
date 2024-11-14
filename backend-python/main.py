57a402c93d027e2f2201b254d5b6ed7aa740ff23
prad-human-007 <kanasepradhumn@gmail.com>
2024-11-14 16:23:24 +0530

    finished the frontend, but track is not getting received

--- backend-python/main.py
+++ backend-python/main.py
@@ -1,6 +1,7 @@
 import asyncio
 from aiortc import RTCPeerConnection, RTCSessionDescription, RTCIceServer, RTCConfiguration,RTCIceCandidate, MediaStreamTrack
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import logging
 
@@ -10,37 +11,73 @@ logger = logging.getLogger("server")
 # create FastAPI app
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all domains
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
+
+class VideoTransformTrack(MediaStreamTrack):
+    """
+    A video stream track that transforms frames from an another track.
+    """
+
+    kind = "video"
+
+    def __init__(self, track):
+        super().__init__()  # don't forget this!
+        self.track = track
+
+    async def recv(self):
+        frame = await self.track.recv()
+
+        return frame
+
 @app.get("/")
 async def read_root():
     return {"message": "Professional Moterboater"}
 
 @app.post("/")
 async def offer(request: Request):
-    params = await request.json()
-    offer = RTCSessionDescription(type=params["type"], sdp=params["sdp"])
+    try:
+        params = await request.json()
+        print(f"Params: \n", params); print("Type of Param: \n", type(params))
+        offer = RTCSessionDescription(type=params["type"], sdp=params["sdp"])
 
-    pc = RTCPeerConnection()
-    logger.info("Created PeerConnection Object for: ", request.client.host)
+        pc = RTCPeerConnection()
+        print("Created PeerConnection Object")
 
-    @pc.on("iceconnectionstatechange")
-    async def on_iceconnectionstatechange():
-        logger.info("ICE connection state is: ", pc.iceConnectionState)
-        if pc.iceConnectionState == "failed":
-            await pc.close()
-            logger.info("PeerConnection closed")
-    
-    @pc.on("track")
-    async def on_track(track: MediaStreamTrack):
-        logger.info(f"Track received: {track} of kind {track.kind}" )
-        pc.addTrack(track)
-    
-    await pc.setRemoteDescription(offer)
-    answer = pc.createAnswer()
-    await pc.setLocalDescription(answer)
+        @pc.on("iceconnectionstatechange")
+        async def on_iceconnectionstatechange():
+            print("ICE connection state is: ", pc.iceConnectionState)
+            if pc.iceConnectionState == "failed":
+                await pc.close()
+                logger.info("PeerConnection closed")
+        
+        @pc.on("track")
+        async def on_track(track):
+            print(f"Track received: {track} of kind {track.kind}" )
+            local_video = VideoTransformTrack(
+                track
+            )
+            pc.addTrack(local_video)
+        
+        await pc.setRemoteDescription(offer)
+        print("Remote Description Set \n",)
 
-    logger.info("Answer: ", pc.localDescription)
+        answer = await pc.createAnswer()
+        print("Answer Created: \n" , )
+        
+        await pc.setLocalDescription(answer)
+        print("Local Description Set: \n", pc.localDescription)
+        
+    except Exception as e:
+        print("Exception: \n", e)
+        # print("Exception: ", e)
 
-    return {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
+    return {"type": pc.localDescription.type, "sdp": pc.localDescription.sdp}
 
 
 
--- frontend/src/components/PythonWS.tsx
+++ frontend/src/components/PythonWS.tsx
@@ -1,106 +1,103 @@
 import { useState, useEffect, useRef } from "react"
 
+const URL = 'http://0.0.0.0:80'
+
 export default function PythonWS() {
     const localVideoRef = useRef<HTMLVideoElement | null>(null);
     const remoteVideoRef = useRef<HTMLVideoElement | null>(null);
-    const wsRef = useRef<WebSocket | null>(null);
     const pcRef = useRef<RTCPeerConnection | null>(null);
-    // const [message, setMessage] = useState<string>("");
-    // const [input, setInput] = useState<string>("");
 
     
     const connect = async () => {
+        pcRef.current = new RTCPeerConnection({
+            iceServers: [{ urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"] }]
+        })
 
-        wsRef.current = new WebSocket("ws://localhost:80/wsRTC");
+        // Add remote stream to video element
+        pcRef.current.addEventListener("track", (event) => {
+            console.log("Received remote stream: ", event.track)
+            const remoteStream = new MediaStream();
+            remoteStream.addTrack(event.track);
+            if(remoteVideoRef.current)
+                remoteVideoRef.current.srcObject = remoteStream;
+        })
+        
+        // Get local media and attach to peer connection
+        await navigator.mediaDevices.getUserMedia({video: true})
+        .then((stream) => {
+            console.log("Got Local Stream")
+            if(localVideoRef.current)
+                localVideoRef.current.srcObject = stream;
+            stream.getTracks().forEach((track) => {
+                pcRef.current?.addTrack(track);
+            })
+        })
+        .catch((error) => {
+            console.log("Error getting local stream: ", error)
+            alert("Error getting local stream: ")
+        })
 
-        wsRef.current.onopen = async () => {
-            console.log("Connected to server");
-
-            pcRef.current = new RTCPeerConnection({
-                iceServers:[ { urls:[ "stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302" ] } ]
-            });
-
-            pcRef.current.onicecandidate = (event) => {
-                if(event.candidate) {
-                    console.log("Sending Ice candidate to server: ", event.candidate, JSON.stringify(event.candidate));
-                    wsRef.current?.send(JSON.stringify({ type: "candidate", candidate: event.candidate}));
+        // Create Offer. Get Ice candidates and send to server
+        await pcRef.current.createOffer()
+        .then((offer) => {
+            console.log("Created Offer: ", offer)
+            pcRef.current?.setLocalDescription(offer);
+            console.log("Local Description Set")
+        })
+        .then(() => {
+            return new Promise<void>((resolve, _) => {
+                if(pcRef.current?.iceGatheringState === "complete") {
+                    resolve();
                 }
-            }
-            
-            pcRef.current.ontrack = (event) => {
-                const vidStream = new MediaStream();
-                vidStream.addTrack(event.track);
-                if(remoteVideoRef.current) {
-                    console.log("Received track: ", event.track);
-                    // console.log("Created Stream: ", vidStream.getTracks());
-                    remoteVideoRef.current.srcObject = vidStream;
-                }
-            }
-
-            await navigator.mediaDevices.getUserMedia({ video: true })
-                .then((stream) => {
-                    console.log("Adding local stream to peer connection");
-                    if(localVideoRef.current) {
-                        localVideoRef.current.srcObject = stream;
-                    }
-                    stream.getTracks().forEach((track) => {
-                        pcRef.current?.addTrack(track);
+                else {
+                    pcRef.current?.addEventListener("icegatheringstatechange", () => {
+                        if(pcRef.current?.iceGatheringState === "complete") {
+                            pcRef.current?.removeEventListener("icegatheringstatechange", () => {});
+                            console.log("Ice Gathering Complete")
+                            resolve();
+                        }
                     });
-                });
-
-            const offer = await pcRef.current.createOffer().then((offer) => {
-                                    console.log("Created offer: ", offer);
-                                    return offer;
-                                });
-            await pcRef.current.setLocalDescription(offer)
-            .then(() => {
-                console.log("local description set");
+                }
             });
-            
-            wsRef.current?.send(JSON.stringify({
-                type: offer.type,
-                sdp: offer.sdp
-            }));
-        }
+        })
+        .then(() => {
+            const offer = pcRef.current?.localDescription;
+            console.log("Sending offer to server: ", offer)
+            return fetch(`${URL}`, {
+                body: JSON.stringify({ 
+                    type: offer?.type,
+                    sdp: offer?.sdp
+                }),
+                headers: {
+                    "Content-type": "application/json"
+                },
+                method: "POST"
+            });
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((answer) => {
+            return pcRef.current?.setRemoteDescription(answer);
+        })
+        .catch((error) => {
+            alert(error)
+            console.log("Error", error)
+        })
 
-        wsRef.current.onmessage = async (event) => {
-            const message = JSON.parse(event.data);
-
-            if (message.type === "answer") {
-                console.log("Received SDP answer from server: ", message);
-                const remoteDesc = new RTCSessionDescription({
-                    type: message.type,
-                    sdp: message.sdp
-                });
-                await pcRef.current?.setRemoteDescription(remoteDesc);
-            } 
-            else if (message.type === "candidate") {
-                console.log("Received ICE candidate from server: ", message);
-                const candidate = new RTCIceCandidate(message.candidate);
-                await pcRef.current?.addIceCandidate(candidate);
-            }
-
-        }
-
-        wsRef.current.onerror = (error) => {
-            console.log("Error in web socket: ", error);
-        }
-
-        wsRef.current.onclose = () => {
-            console.log("Websocket closed");
-        }
+        console.log("Got answer from server: ", pcRef.current?.remoteDescription)
+        console.log("Connected to Server")
 
     }
 
     const disconnect =  () => {
-        pcRef.current?.close();
-        pcRef.current = null;
-        
-        wsRef.current?.close();
-        wsRef.current = null;
+        if(pcRef.current) {
+            pcRef.current.close();
+            pcRef.current = null;
+            console.log("Peer Connection Closed")
+        }
     };
         
-  
 
     return (
         <div className="flex flex-col justify-center items-center">
